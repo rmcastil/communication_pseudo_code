f5ef3d35cb136dad9509cb1af0f1551b3af1fcd0
Ryan Castillo <rmcastil@gmail.com>
2013-03-12 00:38:23 -0400

    add non happy path

--- communication_spec.rb
+++ communication_spec.rb
@@ -1,7 +1,7 @@
 class Sender
 
-  def initialize(said)
-    @said = said
+  def initialize(messages)
+    @messages = messages
   end
 
   def mood
@@ -11,14 +11,19 @@ class Sender
   end
 
   def describe_day_to(another_person)
-    if satisified_with_interpretation?(another_person.interpret(@said))
-      @feel_acknowledged = true
+    @messages.each do |message|
+      if satisified_with_interpretation?(another_person.interpret(message),
+                                         message)
+        @feel_acknowledged = true
+      else
+        @feel_acknowledged = false
+      end
     end
   end
 
-  def satisified_with_interpretation?(interpretation)
-    shared_words = interpretation.split(' ') & @said.split(' ')
-    true if shared_words.size >= 2
+  def satisified_with_interpretation?(interpretation, message)
+    shared_words = interpretation.split(' ') & message.split(' ')
+    true if shared_words.size >= 1
   end
 
 end
@@ -27,15 +32,31 @@ class Recipient
   def interpret(sentance)
     if sentance == "I had a good day"
       "I'm glad you had a good day" 
+    elsif sentance == "Heroku was down"
+      "What is Heroku?"
+    elsif sentance == "There was ruby drama on twitter"
+      "There is drama with ruby? Isn't it a programming language?"
+    elsif sentance == "My presentation isn't done yet"
+      "I'm sorry your presentation is taking so long"
     end
   end
 end
 
 describe Sender do
-  it "is content the listener attempted to understand" do
-    significant_other = Sender.new "I had a good day"
+  it "is in a content mood when understood" do
+    significant_other = Sender.new ["I had a good day"]
     me = Recipient.new
     significant_other.describe_day_to(me)
     significant_other.mood.should >= "content"
   end
+
+  it "is in a content mood when all frustrations are understood" do
+    complaints = ["Heroku was down",
+                  "There was ruby drama on twitter",
+                  "My presentation isn't done yet"]
+    me = Sender.new complaints 
+    significant_other = Recipient.new
+    me.describe_day_to(significant_other)
+    me.mood.should >= "content"
+  end 
 end
