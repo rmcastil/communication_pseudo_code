ad0967e8534d74fffb7f21aeaf6e7eb29ae9185f
Ryan Castillo <rmcastil@gmail.com>
2013-03-11 21:17:31 -0400

    interpretation spec

--- communication_spec.rb
+++ communication_spec.rb
@@ -11,26 +11,31 @@ class Sender
   end
 
   def describe_day_to(another_person)
-    if another_person.listened_to_day?
+    if satisified_with_interpretation?(another_person.interpret(@said))
       @feel_acknowledged = true
     end
+  end
+
+  def satisified_with_interpretation?(interpretation)
+    shared_words = interpretation.split(' ') & @said.split(' ')
+    true if shared_words.size >= 2
   end
 
 end
 
 class Recipient
+  def interpret(sentance)
+    if sentance == "I had a good day"
+      "I'm glad you had a good day" 
+    end
+  end
 end
 
 describe Sender do
-  it "is content the listener listened" do
-    significant_other = Sender.new(message: "I had a good day")
-    me = double(Recipient)
-    me.stub(:listened_to_day?) { true }
-
+  it "is content the listener attempted to understand" do
+    significant_other = Sender.new "I had a good day"
+    me = Recipient.new
     significant_other.describe_day_to(me)
     significant_other.mood.should >= "content"
-  end
-
-  it "is content the listener attempted to understand" do
   end
 end
